e21709aeba6d32585b37dc75c67a5ed2869fc2e1
Mario Babinec <mario.babinec@unitedclassifieds.sk>
2023-11-14 10:56:54 +0100

    fix: hotfix css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import logoImage from '../assets/logo.png';
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className={''}>
                 <a className="navbar-brand fw-bold justify-center align-items-center" href="/">
                     <img src={logoImage} alt="Logo" width="30" height="30" className="d-inline-block align-top mr-2"/>
