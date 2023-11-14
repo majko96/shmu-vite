6a85a0b276acbc09f90219b4212a6debdb73a0b6
Mario Babinec <mario.babinec@unitedclassifieds.sk>
2023-11-14 10:29:22 +0100

    fix: fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import logoImage from '../assets/logo.png';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className={'container px-5'}>
+            <div className={''}>
                 <a className="navbar-brand fw-bold justify-center align-items-center" href="/">
                     <img src={logoImage} alt="Logo" width="30" height="30" className="d-inline-block align-top mr-2"/>
                     Radiation
